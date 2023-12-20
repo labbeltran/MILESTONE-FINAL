8e9c7eeba1cc6de195ae31c881be5b7832386887
labbeltran <labbeltran@gmail.com>
2023-12-19 21:12:38 -0500

    added js file; consider generating lv2 and lv3 maze

---
+++ Assets/JSS/index.js
@@ -0,0 +1,43 @@
+let levelOne =[
+  [1,0,1,0,1],
+  [1,0,1,1,1],
+  [1,1,0,1,1],
+  [0,1,1,1,0],
+  [1,1,1,0,1],
+]
+
+let mazeArray = level1;
+  let maze = document.getElementById('maze-container')
+  let tank = document.getElementbyId('labyrinth-tank')
+  let exit = document.getElementbyId('pearly-gates')
+  
+  // ----Create maze------
+  function createMaze(){
+    for (let i = 0; i< mazeArray.length; i++){
+      let row = document.getElementById('div');
+      row.classList.add('row');
+      for(let j = 0; j< mazeArray[i].length; j++){
+        let cell= document.createElement('div');
+        cell.classList.add('cell');
+      
+        if(mazeArray[i][j] === 0){
+          cell.classList.add('wall');
+        }
+        row.appendChild(cell);
+      }
+      maze.appendChild(row)
+    }
+    
+//     ----Function to create maze on button click----
+    // async function(){
+    //   await 
+    // }
+    
+  // ----Function that lets player move through maze
+  
+  // ------Function that checks if player entered exit cell___
+  
+  
+  // ----Function that generates timer-------
+  
+  // ----Function that compares time and returns score based on time left----------
